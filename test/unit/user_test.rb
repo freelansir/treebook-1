2d20c901900bc143989178a1148be2fbc0aef394
David Kong <dave.a.kong@gmail.com>
2012-12-30 13:34:42 -0800

    Added lots - profile pages, tests etc...

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -28,12 +28,23 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "a user should have a profile name without spaces" do
-		user = User.new
+		user = User.new(first_name: 'David', last_name: 'Kong', email: 'dave.a.kong2@gmail.com')
+		user.password = user.password_confirmation = 'password'
+
 		user.profile_name = "My Profile With Spaces"
 
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
+
+	test "a user can have a correctly formatted profile name" do
+		user = User.new(first_name: 'David', last_name: 'Kong', email: 'dave.a.kong2@gmail.com')
+		user.password = user.password_confirmation = 'password'
+		
+		user.profile_name = 'freelansir2'
+
+		assert user.valid?
 	end
 
 end
